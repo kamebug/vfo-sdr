51a987ca2a2301df16b7f4f5ac3b2f20df06b6de
kamebug <kamebug@gmail.com>
2026-07-31 07:38:53 +0900

    enriquece VOR/ILS, AIS, ADS-B e recursos com dicas do manual

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // Incrementar CACHE_VERSION a cada mudança no index.html/manifest/icon
-const CACHE_VERSION = 'vfo-v4';
+const CACHE_VERSION = 'vfo-v5';
 const ASSETS = [
   './',
   './index.html',
